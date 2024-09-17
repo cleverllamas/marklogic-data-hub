f0ab72dbf74eb4c24607e2a775515b2cbfd7743a
David Ennis - A Clever Llama <david.ennis@cleverllamas.com>
2024-09-17 22:25:41 +0100

    code complete

--- marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/flows/run/flowRunXquery.mjs
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/flows/run/flowRunXquery.mjs
@@ -17,12 +17,25 @@ function xqueryTest() {
   const content = datahub.flow.findMatchingContent("CustomerByValue", "1", options);
 
   xdmp.log(["DAE - matchingContent", content])
-
+  const uri  = "/processed/customer1-xquery.json"
   assertions.push(
     test.assertEqual(1, content.length),
-    test.assertEqual("test-data", content[0].uri,
-      "Each value is stored in a separate content item under the 'uri' property so that we can maintain the convention " +
-      "that every content item has a 'uri' property.")
+    test.asserttrue(xdmp.documentGetCollections(uri).toString()  == ["value-collection", "CustomerByValue", "test-data-xqy"].toString(), "same collections"),
+    test.asserttrue(JSON.stringify({ ... xdmp.documentGetPermissions(uri),}) == '{"0":{"capability":"update","roleId":"7004461930022123088"},"1":{"capability":"read","roleId":"7004461930022123088"}}', "permissions"),
+       test.asserttrue(
+      JSON.stringify({ ... xdmp.documentGetMetadata(uri), ... {"datahubCreatedOn" : null, "datahubCreatedBy": null}})
+      == 
+      JSON.stringify({
+      "datahubCreatedBy": null, 
+      "datahubCreatedOn": null, 
+      "some-key": "some-value", 
+      "datahubCreatedInFlow": "CustomerByValueXquery", 
+      "key1": "1", 
+      "datahubRanBySteps": "", 
+      "datahubCreatedByJob": "value-test-job-xquery"
+      })  
+      , "same collections"
+    )
   );
 
   const response = datahub.flow.runFlow(flowName, 'value-test-job-xquery', content, options, 1);
--- marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/flows/run/suite-teardown.xqy
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/flows/run/suite-teardown.xqy
@@ -1,23 +1,26 @@
 xquery version "1.0-ml";
-
-xdmp:invoke-function(function() {
-  xdmp:document-delete(
-    "/test/custom-null-step/main.mjs"
-  ),
+(
+  xdmp:invoke-function(function() {
     xdmp:document-delete(
-    "/test/custom-by-value-step/main.mjs"
+      "/test/custom-null-step/main.mjs"
+    ),
+      xdmp:document-delete(
+      "/test/custom-by-value-step/main.mjs"
+    ),
+      xdmp:document-delete(
+      "/test/custom-by-value-step/main.xqy"
+    )
+  },
+    map:entry("database", xdmp:modules-database())
   ),
-    xdmp:document-delete(
-    "/test/custom-by-value-step/main.xqy"
-  )
-},
-  map:entry("database", xdmp:modules-database())
-);
 
-xdmp:invoke-function(function() {
-  xdmp:document-delete(
-    "/processed/customer1-xquery.json"
+
+  xdmp:invoke-function(function() {
+    xdmp:document-delete(
+      "/processed/customer1-xquery.json"
+    )
+  },
+    map:entry("database", "data-hub-FINAL")
   )
-},
-  map:entry("database", "data-hub-FINAL")
+
 )
