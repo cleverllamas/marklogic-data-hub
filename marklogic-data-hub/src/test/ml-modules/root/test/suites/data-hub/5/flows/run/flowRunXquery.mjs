95236733ef31989c6c506f4d68bc10de850c7aeb
David Ennis - A Clever Llama <david.ennis@cleverllamas.com>
2024-09-17 22:34:59 +0100

    code complete

--- marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/flows/run/flowRunXquery.mjs
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/flows/run/flowRunXquery.mjs
@@ -18,26 +18,27 @@ function xqueryTest() {
 
   xdmp.log(["DAE - matchingContent", content])
   xdmp.invokeFunction(function(){
-  const uri  = "/processed/customer1-xquery.json"
-  assertions.push(
-    test.assertEqual(1, content.length),
-    test.assertTrue(xdmp.documentGetCollections(uri).toString()  == ["value-collection", "CustomerByValue", "test-data-xqy"].toString(), "same collections"),
-    test.assertTrue(JSON.stringify({ ... xdmp.documentGetPermissions(uri),}) == '{"0":{"capability":"update","roleId":"7004461930022123088"},"1":{"capability":"read","roleId":"7004461930022123088"}}', "permissions"),
-    test.assertTrue(
-      JSON.stringify({ ... xdmp.documentGetMetadata(uri), ... {"datahubCreatedOn" : null, "datahubCreatedBy": null}})
-      == 
-      JSON.stringify({
-      "datahubCreatedBy": null, 
-      "datahubCreatedOn": null, 
-      "some-key": "some-value", 
-      "datahubCreatedInFlow": "CustomerByValueXquery", 
-      "key1": "1", 
-      "datahubRanBySteps": "", 
-      "datahubCreatedByJob": "value-test-job-xquery"
-      })  
-      , "same collections"
-    )
-  );
+    xdmp.log(["DAE - doc", cts.doc(uri)])
+    const uri  = "/processed/customer1-xquery.json"
+    assertions.push(
+      test.assertEqual(1, content.length),
+      test.assertTrue(xdmp.documentGetCollections(uri).toString()  == ["value-collection", "CustomerByValue", "test-data-xqy"].toString(), "same collections"),
+      test.assertTrue(JSON.stringify({ ... xdmp.documentGetPermissions(uri),}) == '{"0":{"capability":"update","roleId":"7004461930022123088"},"1":{"capability":"read","roleId":"7004461930022123088"}}', "permissions"),
+      test.assertTrue(
+        JSON.stringify({ ... xdmp.documentGetMetadata(uri), ... {"datahubCreatedOn" : null, "datahubCreatedBy": null}})
+        == 
+        JSON.stringify({
+        "datahubCreatedBy": null, 
+        "datahubCreatedOn": null, 
+        "some-key": "some-value", 
+        "datahubCreatedInFlow": "CustomerByValueXquery", 
+        "key1": "1", 
+        "datahubRanBySteps": "", 
+        "datahubCreatedByJob": "value-test-job-xquery"
+        })  
+        , "same collections"
+      )
+    );
   }, {"database" : xdmp.database("data-hub-FINAL")})
 
   const response = datahub.flow.runFlow(flowName, 'value-test-job-xquery', content, options, 1);
